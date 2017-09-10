1c4537891c255c6f0162c03d76058e95f0a4a41d
main <476515936@qq.com>
2017-09-10 16:01:02 +0800

    init

---
+++ provide-test/src/main/java/com/zjw/provide/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.zjw.provide.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "test controller interface, time:"+System.currentTimeMillis();
+	}
+	
+}
